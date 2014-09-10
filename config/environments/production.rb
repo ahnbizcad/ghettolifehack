2338a610bac1b4dc0689578f2fdd4200ba4d8d5e
ahnbizcad <ahnbizcad@gmail.com>
2014-09-10 12:17:25 -0700

    css.scss manifest, keys, settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,16 @@ Rails.application.configure do
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   config.assets.precompile += %w( search.js )
 
-  # Required for Devise
-  config.action_mailer.default_url_options = { host: 'ghettolifehack.com', port:3000 }
+#
+  
+  # Disable Rails's static asset server (Apache or nginx will already do this).
+  config.serve_static_assets = true #This is bad and slow. Only a temporary solution.
+
+#
+
+  # Specifies the header that your server uses for sending files.
+  config.action_dispatch.x_sendfile_header = nil # for heroku
+  #config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
+  #config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
 end
